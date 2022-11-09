7356cb242b5adc56e9bbc877f75bff9a8348c62a
Facundo Pastor <facupastor.dev@gmail.com>
2022-11-09 10:53:14 -0300

    preEntrega2

--- src/Componentes/NavBar.js
+++ src/Componentes/NavBar.js
@@ -2,7 +2,7 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import CartWidget from './CartWidget';
-import NavDropdown from 'react'
+import NavDropdown from 'react-bootstrap/NavDropdown'
 import { Link, NavLink } from 'react-router-dom'
 
 function NavBar() {
@@ -18,21 +18,33 @@ function NavBar() {
             </div>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav >
-                        <ul className="me-auto">
-                            <li>                        
-                                <NavLink to={`/`} className="links">Inicio</NavLink></li>
-                            <li>                        
-                                <NavLink to={`/marca/Adidas`} className="links">Adidas</NavLink></li>
-                            <li>                        
-                                <NavLink to={`/marca/New Balance`} className="links">New Balanace</NavLink></li>
-                            <li>                        
-                                <NavLink to={`/marca/Nike`} className="links">Nike</NavLink></li>
-                            <li>                        
-                                <NavLink to={`/marca/Puma`} className="links">Puma</NavLink></li>
-                            <li>                        
-                                <NavLink to={`/marca/Vans`} className="links">Vans</NavLink></li>
-                        </ul>
+                    <Nav>
+                
+                                                 
+                                <NavLink to={`/`} className="links">Inicio</NavLink>
+                         
+                            <NavDropdown className="links" title="MARCAS" id="basic-nav-dropdown">
+                                <NavDropdown.Item className="links marcas" href="#action/3.1">                           
+                                    <li>                        
+                                        <NavLink to={`/marca/Adidas`} className="links">Adidas</NavLink>
+                                    </li>
+                                    <li>                        
+                                        <NavLink to={`/marca/New Balance`} className="links">New Balanace</NavLink>
+                                    </li>
+                                    <li>                        
+                                        <NavLink to={`/marca/Nike`} className="links">Nike</NavLink>
+                                    </li>
+                                    <li>                        
+                                        <NavLink to={`/marca/Puma`} className="links">Puma</NavLink>
+                                    </li>
+                                    <li>                        
+                                        <NavLink to={`/marca/Vans`} className="links">Vans</NavLink>
+                                    </li>
+                                </NavDropdown.Item>
+                            </NavDropdown>
+ 
+                       
+
                     </Nav>
                 </Navbar.Collapse>
                 <CartWidget/>
